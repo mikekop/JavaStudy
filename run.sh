d4e17f310f862d6e19073038cd0589a41ca91fd4
Alexander Panyukov <olenevodec@gmail.com>
2018-02-13 23:16:25 +0300

    Fix run script for multiple packages

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-javac -d out ./src/ru/logrocon/lesson1/kitchen/Kitchen.java
+javac -sourcepath ./src -d out ./src/ru/logrocon/lesson1/kitchen/Kitchen.java &&
 java -classpath ./out ru.logrocon.lesson1.kitchen.Kitchen
