4051f0419df0b45c0162153dfcc8d3496bdf89f2
Alexander Panyukov <stydent@hotmail.com>
2018-02-13 21:40:00 +0300

    First test commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+javac -d out ./src/ru/logrocon/lesson1/kitchen/Kitchen.java
+java -classpath ./out ru.logrocon.lesson1.kitchen.Kitchen
---
+++ src/ru/logrocon/lesson1/kitchen/Kitchen.java
@@ -0,0 +1,8 @@
+package ru.logrocon.lesson1.kitchen;
+
+public class Kitchen {
+
+    public static void main(String[] args) {
+        System.out.println("Привет, кухня!");
+    }
+}
