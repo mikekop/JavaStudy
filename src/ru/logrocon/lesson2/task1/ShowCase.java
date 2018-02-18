4a76903d6104de0406d2fb7ec42511c1ec56a2d1
amatkov <man333@mail.ru>
2018-02-19 01:30:48 +0300

    Метод оптимизации

---
+++ src/ru/logrocon/lesson2/task1/ShowCase.java
@@ -0,0 +1,7 @@
+package ru.logrocon.lesson2.task1;
+
+public class ShowCase {
+    public ShowCase(){
+
+    }
+}
---
+++ src/ru/logrocon/lesson2/task2/ConveyerTask.java
@@ -0,0 +1,7 @@
+package ru.logrocon.lesson2.task2;
+
+public class ConveyerTask {
+    public ConveyerTask(){
+
+    }
+}
